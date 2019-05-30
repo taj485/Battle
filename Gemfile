39cd9de02f28dcc102e2027495b2990ac7c1e574
Megan Goode <megan.e.goode@hotmail.com>
2019-05-30 11:43:27 +0100

    Add play view and get player names

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rspec'
 gem 'sinatra'
-gem 'shotgun'
 gem 'capybara'
 gem 'selenium-webdriver'
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,17 @@
-# require '../app.rb'
 
 feature 'Testing infrastructure' do
-scenario 'Can run app and check page content' do
-  visit('/')
-  expect(page).to have_content 'Testing infrastructure working!'
+  scenario 'Can run app and check page content' do
+    # visit('/')
+    # expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
+
+feature 'Add player names' do
+  scenario 'Can submit a form with names and check names are on screen' do
+    visit('/')
+    fill_in 'player_1_name', with: 'Player1'
+    fill_in 'player_2_name', with: 'Player2'
+    click_button 'Submit'
+    expect(page).to have_content 'Player1 vs Player2'
   end
 end
