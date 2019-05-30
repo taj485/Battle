021594f16c931851d4997f3352dc8150f24efb29
Megan Goode <megan.e.goode@hotmail.com>
2019-05-30 15:28:07 +0100

    Add player 2 hit points

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -7,4 +7,14 @@ feature 'Enter player names' do
     click_button 'Submit'
     expect(page).to have_content 'Player1 vs Player2'
   end
+
+  feature 'Player 2\'s points' do
+    scenario 'Can see player 2\'s hit points' do
+      visit('/')
+      fill_in 'player_1_name', with: 'Player1'
+      fill_in 'player_2_name', with: 'Player2'
+      click_button 'Submit'
+      expect(page).to have_content 'Player2\'s hit points: 10'
+    end
+  end
 end
