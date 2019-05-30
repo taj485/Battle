def825d16f8ecc9541b5fb422d5d0e9945b82a64
Taj <tajbi21@gmail.com>
2019-05-30 15:12:03 +0100

    session is now working

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-enable :session
+enable :sessions
 
 
   get '/' do
-    erb(:index)
+    erb :index
   end
 
   post '/names' do
@@ -15,9 +15,9 @@ enable :session
   end
 
   get '/play' do
-    @player1= session[:player_1_name]
-    @player2= session[:player_2_name]
-    erb(:play)
+    @player_1_name= session[:player_1_name]
+    @player_2_name= session[:player_2_name]
+    erb :play
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
